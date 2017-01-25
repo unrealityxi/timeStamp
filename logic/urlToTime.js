88920c20fdb25ccc766f62ec0710c21c0e9f9caf
Dragan Roganović <unrealityxi@gmail.com>
2017-01-25 21:37:08 +0000

    Finished basic API

---
+++ logic/urlToTime.js
@@ -0,0 +1,17 @@
+
+var moment = require("moment");
+
+function app(query){
+
+  var momentDate = isNaN(query) ? moment(query) : moment.unix(query);
+  
+  var unix;
+  var natural;
+  natural = momentDate.format("MMMM D, YYYY");
+  (natural == "Invalid date") ? natural = null : null;
+  unix = momentDate.unix();
+  
+  return JSON.stringify({"unix": unix, "natural": natural});
+}
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,10 +1,38 @@
 var express = require("express");
+var getJsonDate = require("./logic/urlToTime");
+
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get("/", function(req, res){
+ 
+  res.send("Default page");
+});
+
+app.get("/*", function(req, res, next){
+  // First decode uri. Might throw an error. 
+  var query = decodeURIComponent(req.url).slice(1);
+  
+  // outsource headache to prewritten function
+  var json = getJsonDate(query);
+  
+  // tadah
+  res.setHeader('Content-Type', 'application/json');
+  res.send(json);
+  
+});
+
+
+
+// Handle errors.
+app.use(function(err, req, res, next) {
+    // Log error for future generations
+    console.log(err);
+    //subtly hint user that something blew
+    return res.send("Bad data!");
 });
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
+
+
