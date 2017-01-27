1f389f4c641aa6d7a17c5312a924185314bd8c2e
Dragan Roganović <unrealityxi@gmail.com>
2017-01-27 16:34:45 +0100

    Refactored

--- logic/urlToTime.js
+++ logic/urlToTime.js
@@ -2,18 +2,26 @@
 var moment = require("moment");
 
 function app(query){
-
-
-
-  var momentDate = isNaN(query) ? moment(new Date(query)) : moment.unix(query);
+  // Check if we got unix or natural date and create date acordingly
+  var query = isNaN(query) ? new Date(query) : new Date(query * 1000);
   
+  var momentDate = moment(new Date(query));
+
   var unix;
   var natural;
-  natural = momentDate.format("MMMM D, YYYY");
-  (natural == "Invalid date") ? natural = null : null;
-  unix = momentDate.unix();
-  
-  return JSON.stringify({"unix": unix, "natural": natural});
+  if (momentDate.isValid()){
+    natural = momentDate.format("MMMM D, YYYY")
+    unix = momentDate.unix();
+  }
+  else {
+    natural = null
+    unix = null;
+  }
+
+  return JSON.stringify({
+                          "unix": unix, 
+                          "natural": natural
+                        });
 }
 
 module.exports = app;
